4500b9e5f7505e2a4762fdaf75527c673e5589a8
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-06-04 19:28:17 +0530

    Updated the UNINSTALL shell script

--- .bluemix/pipeline-UNINSTALL.sh
+++ .bluemix/pipeline-UNINSTALL.sh
@@ -5,7 +5,6 @@ helm delete mmssearch --purge
 
 # delete the secrets stored in our cluster
 kubectl delete secret mms-secret
-kubectl delete secret petstore-docker-registry
 
 # remove the images
 ibmcloud cr image-rm $DB_IMAGE_URL
